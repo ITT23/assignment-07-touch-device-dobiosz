41019f2b245231ecec61f1fa677812d102107e00
MatthiasDobiosz <matthido98@web.de>
2023-06-23 19:48:13 +0200

    basic image loading

---
+++ multitouch-demo.py
@@ -0,0 +1,72 @@
+import pyglet
+from PIL import Image
+import random
+
+# Load images
+stairs = pyglet.image.load('./img/stairs.jpg')
+tables = pyglet.image.load('./img/tables.jpg')
+windows = pyglet.image.load('./img/windows.jpg')
+images = [stairs, tables, windows]
+
+
+class ChangeableImage:
+
+    def __init__(self, originalImage, x, y):
+        self.currentImage = originalImage
+        self.width = originalImage.width
+        self.height = originalImage.height
+        self.rotation = 0
+        self.x = x
+        self.y = y
+
+    def rescale(self, factor):
+        self.width = int(self.width * factor)
+        self.height = int(self.height * factor)
+        self.currentImage.width = self.width
+        self.currentImage.height = self.height
+
+    def rotate(self):
+        print("test")
+        # TODO: Rotate image
+
+
+class ImageManipulator:
+
+    def __init__(self):
+        self.minSize = 0.1
+        self.maxSize = 0.5
+        self.changeableImages = []
+        self.init_images_randomly()
+
+    def init_images_randomly(self):
+        for image in images:
+            changeableImage = ChangeableImage(image, 0, 0)
+            scale = random.randint(1, 10)
+            changeableImage.rescale(1/scale)
+            self.changeableImages.append(changeableImage)
+            changeableImage.currentImage.blit(0, 0)
+
+    def display_images(self):
+        for image in self.changeableImages:
+            image.currentImage.blit(0, 0)
+
+
+display = pyglet.canvas.get_display()
+screens = display.get_screens()
+window = pyglet.window.Window(fullscreen=True, screen=screens[0])
+
+imageMan = ImageManipulator()
+
+
+@window.event
+def on_draw():
+    window.clear()
+    imageMan.display_images()
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == pyglet.window.key._3:
+        pyglet.app.exit()
+
+
+pyglet.app.run()
--- touch-input.py
+++ touch-input.py
@@ -1,18 +1,31 @@
 import cv2
 import sys
+import numpy as np
+from numpy.linalg import norm
 
 video_id = 1
 
 if len(sys.argv) > 1:
     video_id = int(sys.argv[1])
 
-cap = cv2.VideoCapture(video_id)
+cap = cv2.VideoCapture('2023-06-19 17-48-47.mkv')
 cv2.namedWindow('testwind')
+
+
+# https://stackoverflow.com/questions/14243472/estimate-brightness-of-an-image-opencv
+def brightness(img):
+    if len(img.shape) == 3:
+        # Colored RGB or BGR (*Do Not* use HSV images with this function)
+        # create brightness with euclidean norm
+        return np.average(norm(img, axis=2)) / np.sqrt(3)
+    else:
+        # Grayscale
+        return np.average(img)
 
 
 def process_image(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(gray, 125, 255, cv2.THRESH_BINARY)
 
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,
                                            cv2.CHAIN_APPROX_SIMPLE)
@@ -22,8 +35,8 @@ def process_image(img):
         for cnt in contours:
             area = cv2.contourArea(cnt)
             x, y, w, h = cv2.boundingRect(cnt)
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        # cv2.drawContours(img, [contours], -1, (255, 255, 0), 2)
+            # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.drawContours(img, [contours], -1, (255, 255, 0), 2)
 
     return img
 
@@ -31,10 +44,9 @@ def process_image(img):
 while True:
     success, frame = cap.read()
     if success:
-        # frame = process_image(frame)
+        print(brightness(frame))
+        frame = process_image(frame)
         cv2.imshow('testwind', frame)
-        #height, width, channels = frame.shape
-        #out = cv2.VideoWriter('test.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 20.0, (width, height))
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
